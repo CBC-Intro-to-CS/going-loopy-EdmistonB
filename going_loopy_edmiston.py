4ab8f30eb09b613a2dff74e8850d31557725118d
Bryce <edmistonb@cbchs.org>
2024-09-19 20:58:38 -0500

    loops

---
+++ going_loopy_edmiston.py
@@ -0,0 +1,21 @@
+"""
+age = int(input("Enter your age?"))
+for x in range(1, 17, 2):
+    print(x)
+
+"""
+
+
+"""
+sandwhiches = ["ham", "turkey", "cheese", "mayonaise", "lettuce"]
+for sandwhich in sandwhiches:
+    print(f"Next is {sandwhich}")
+
+print("Finished with your sandwhich")
+"""
+
+weight = 165
+for year in range(1, 16):
+    weight = weight + 1
+    moon_weight = weight * 0.165
+    print(f"Year {year} is {moon_weight}")
